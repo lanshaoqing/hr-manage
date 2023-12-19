f8468e4b6256082a8f909f96dc3220968f235d82
Lan <2651537541@qq.com>
2023-12-19 17:12:59 +0800

    获取角色列表

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,12 +1,45 @@
 <template>
   <div class="container">
     <div class="app-container">
-      角色管理
+      <div class="role-operate">
+        <el-button type="primary" size="mini">添加角色</el-button>
+      </div>
+      <el-table :data="list">
+        <el-table-column prop="name" label="角色" align="center" width="200" />
+        <el-table-column prop="state" label="启用" align="center" width="200" />
+        <el-table-column prop="description" label="描述" align="center" />
+        <el-table-column label="操作" align="center" />
+      </el-table>
+      <el-row type="flex" style="height: 60px;" align="middle" justify="end">
+        <el-pagination
+          layout="prev, pager, next"
+        />
+      </el-row>
     </div>
   </div>
 </template>
 <script>
+import { getRoleList } from '@/api/role'
 export default {
-  name: 'Role'
+  name: 'Role',
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getRoleList()
+  },
+  methods: {
+    async getRoleList() {
+      const { rows } = await getRoleList()
+      this.list = rows
+    }
+  }
 }
 </script>
+<style scoped>
+.role-operate{
+  padding:10px;
+}
+</style>
