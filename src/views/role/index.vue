c0df14e364b44d396dc8228d99cd39bf5506fb6a
Lan <2651537541@qq.com>
2023-12-19 17:27:57 +0800

    分页

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -6,13 +6,27 @@
       </div>
       <el-table :data="list">
         <el-table-column prop="name" label="角色" align="center" width="200" />
-        <el-table-column prop="state" label="启用" align="center" width="200" />
+        <el-table-column prop="state" label="启用" align="center" width="200">
+          <template v-slot="{row}">
+            <span>{{ row.state===1 ? '已启用': row.state===0 ? '未启用':'0' }}</span>
+          </template>
+        </el-table-column>
         <el-table-column prop="description" label="描述" align="center" />
-        <el-table-column label="操作" align="center" />
+        <el-table-column label="操作" align="center">
+          <template>
+            <el-button size="mini" type="text">分配权限</el-button>
+            <el-button size="mini" type="text">编辑</el-button>
+            <el-button size="mini" type="text">删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <el-row type="flex" style="height: 60px;" align="middle" justify="end">
         <el-pagination
           layout="prev, pager, next"
+          :total="pageParams.total"
+          :page-size="pageParams.pagesize"
+          :current-page="pageParams.page"
+          @current-change="pageChange"
         />
       </el-row>
     </div>
@@ -24,7 +38,12 @@ export default {
   name: 'Role',
   data() {
     return {
-      list: []
+      list: [],
+      pageParams: {
+        page: 1,
+        pagesize: 5,
+        total: 0
+      }
     }
   },
   created() {
@@ -32,8 +51,13 @@ export default {
   },
   methods: {
     async getRoleList() {
-      const { rows } = await getRoleList()
+      const { rows, total } = await getRoleList(this.pageParams)
       this.list = rows
+      this.pageParams.total = total
+    },
+    pageChange(newpage) {
+      this.pageParams.page = newpage
+      this.getRoleList()
     }
   }
 }
