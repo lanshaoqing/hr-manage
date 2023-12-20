a17ab28f6e988779df08e9c70ad4d775b5ff7aca
Lan <2651537541@qq.com>
2023-12-20 14:44:31 +0800

    员工表格信息显示

---
+++ src/api/employee.js
@@ -0,0 +1,8 @@
+import request from '@/utils/request'
+
+export function getEmployeeList(params) {
+  return request({
+    url: '/sys/user',
+    params // 地址参数 查询参数
+  })
+}
--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -2,9 +2,24 @@
   <div class="container">
     <div class="app-container">
       <div class="left">
-        <el-input style="margin-bottom:10px" type="text" prefix-icon="el-icon-search" size="small" placeholder="输入员工姓名全员搜索" />
+        <el-input
+          style="margin-bottom:10px"
+          type="text"
+          prefix-icon="el-icon-search"
+          size="small"
+          placeholder="输入员工姓名全员搜索"
+        />
         <!-- 树形组件 -->
-        <el-tree :data="depts" :props="defaultPrpps" default-expand-all :expand-on-click-node="false" highlight-current />
+        <el-tree
+          ref="deptTree"
+          :data="depts"
+          :props="defaultPrpps"
+          default-expand-all
+          :expand-on-click-node="false"
+          highlight-current
+          node-key="id"
+          @current-change="selectNode"
+        />
       </div>
       <div class="right">
         <el-row class="opeate-tools" type="flex" justify="end">
@@ -13,7 +28,37 @@
           <el-button size="mini">excel导出</el-button>
         </el-row>
         <!-- 表格组件 -->
+        <el-table :data="list">
+          <el-table-column prop="staffPhoto" align="center" label="头像">
+            <template v-slot="{row}">
+              <el-avatar v-if="row.staffPhoto" :src="row.staffPhoto" :size="30" />
+              <span v-else class="username">{{ row.username.charAt(0) }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="username" label="姓名" />
+          <el-table-column prop="mobile" label="手机号" sortable />
+          <el-table-column prop="workNumber" label="工号" sortable />
+          <el-table-column prop="formOfEmployment" label="聘用形式">
+            <template v-slot="{row}">
+              <span v-if="row.formOfEmployment===1">正式</span>
+              <span v-else-if="row.formOfEmployment ===2">非正式</span>
+              <span v-else>无</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="departmentName" label="部门" />
+          <el-table-column prop="timeOfEntry" label="入职时间" sortable />
+          <el-table-column label="操作" width="280px">
+            <template>
+              <el-button size="mini" type="text">查看</el-button>
+              <el-button size="mini" type="text">角色</el-button>
+              <el-button size="mini" type="text">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
         <!-- 分页 -->
+        <el-row style="height: 60px;" align="middle" type="flex" justify="end">
+          <el-pagination layout="total,prev,pager,next" :total="1000" />
+        </el-row>
       </div>
     </div>
   </div>
@@ -21,6 +66,7 @@
 
 <script>
 import { getDepartment } from '@/api/department'
+import { getEmployeeList } from '@/api/employee'
 import { transListToTreeData } from '@/utils'
 export default {
   name: 'Employee',
@@ -30,7 +76,11 @@ export default {
       defaultPrpps: {
         label: 'name',
         children: 'children'
-      }
+      },
+      queryParams: {
+        departmentId: null
+      },
+      list: []
     }
   },
   created() {
@@ -40,6 +90,19 @@ export default {
     async getDepartment() {
       const result = await getDepartment()
       this.depts = transListToTreeData(result, 0)
+      this.queryParams.departmentId = this.depts[0].id
+      this.$nextTick(() => {
+        this.$refs.deptTree.setCurrentKey(this.queryParams.departmentId)
+      })
+      this.getEmployeeList()
+    },
+    selectNode(node) {
+      this.queryParams.departmentId = node.id
+      this.getEmployeeList()
+    },
+    async getEmployeeList() {
+      const { rows } = await getEmployeeList(this.queryParams)
+      this.list = rows
     }
   }
 
@@ -50,17 +113,21 @@ export default {
 .app-container {
   background: #fff;
   display: flex;
+
   .left {
     width: 280px;
     padding: 20px;
     border-right: 1px solid #eaeef4;
   }
+
   .right {
     flex: 1;
     padding: 20px;
+
     .opeate-tools {
-      margin:10px ;
+      margin: 10px;
     }
+
     .username {
       height: 30px;
       width: 30px;
@@ -70,9 +137,8 @@ export default {
       color: #fff;
       background: #04C9BE;
       font-size: 12px;
-      display:inline-block;
+      display: inline-block;
     }
   }
 }
-
 </style>
