ead4cbf6ea7e5c56b18565183348051c0e59515a
Lan <2651537541@qq.com>
2023-12-21 12:06:48 +0800

    使用cos完成图片上传

--- src/views/employee/components/image-upload.vue
+++ src/views/employee/components/image-upload.vue
@@ -4,6 +4,7 @@
     action=""
     :show-file-list="false"
     :before-upload="beforeAvatarUpload"
+    :http-request="uploadImage"
   >
     <!-- (自动上传)action是上传地址 人资项目不需要 人资项目(手动上传)  -->
     <!-- show-file-list不展示列表 -->
@@ -13,6 +14,7 @@
 </template>
 
 <script>
+import COS from 'cos-js-sdk-v5'
 export default {
   props: {
     value: {
@@ -35,12 +37,31 @@ export default {
         this.$message.error('上传头像图片大小不能超过 5MB!')
       }
       return isJPG && isLt2M
+    },
+    uploadImage(params) {
+      const cos = new COS({
+        SecretId: 'AKIDKVTDk5IIZdUTKwQh0Xs0vc7D7nGUXN5b',
+        SecretKey: 'ZvvkNp5A1e3MVSDk5GEtNdhYBaq0ykLl'
+      })// cos腾讯云对象初始化
+      cos.putObject({
+        Bucket: 'lanshaoqing-1323291221',
+        Region: 'ap-beijing',
+        Key: params.file.name,
+        StorageClass: 'STANDARD',
+        Body: params.file
+      }, (err, data) => {
+        if (data.statusCode === 200 && data.Location) {
+          this.$emit('input', `http://${data.Location}`)
+        } else {
+          this.$message.error(err.message)
+        }
+      })
     }
   }
 }
 </script>
 
-  <style>
+<style>
     .avatar-uploader .el-upload {
       border: 1px dashed #d9d9d9;
       border-radius: 6px;
